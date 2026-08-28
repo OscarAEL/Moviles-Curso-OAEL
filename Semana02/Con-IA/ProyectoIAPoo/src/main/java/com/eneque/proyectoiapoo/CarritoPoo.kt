845267f1247607faee511e8b816daebee3edce6d
OscarAEL <oscar.eneque.l@tecsup.edu.pe>
2026-08-28 00:02:19 -0500

    Creacion clase Producto con encapsulamiento

---
+++ Semana02/Con-IA/ProyectoIAPoo/src/main/java/com/eneque/proyectoiapoo/CarritoPoo.kt
@@ -0,0 +1,25 @@
+package com.eneque.proyectoiapoo
+
+data class Producto(
+    val id: String,
+    val nombre: String,
+    val precio: Double,
+    private var _stock: Int
+) {
+    val stock: Int get() = _stock
+
+    fun tieneStock(cantidad: Int): Boolean = _stock >= cantidad
+
+    fun reducirStock(cantidad: Int) {
+        if (!tieneStock(cantidad)) throw IllegalArgumentException("Stock insuficiente para $nombre")
+        _stock -= cantidad
+    }
+
+    fun reponerStock(cantidad: Int) {
+        _stock += cantidad
+    }
+}
+
+fun main() {
+    println("Proyecto POO - Carrito de Compras (con IA)")
+}
