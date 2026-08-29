14befa22f1cffac32a57d795eb8eee28484c4cdf
OscarAEL <oscar.eneque.l@tecsup.edu.pe>
2026-08-28 20:13:07 -0500

    Se agrega ingreso de datos de matricula

---
+++ Semana02/Lab02Viernes/app/src/main/java/com/eneque/lab02viernes/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.eneque.lab02viernes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eneque.lab02viernes.ui.theme.Lab02ViernesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab02ViernesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab02ViernesTheme {
+        Greeting("Android")
+    }
+}
---
+++ Semana02/Lab02Viernes/lab02viernes/src/main/java/com/eneque/lab02viernes/Matricula.kt
@@ -0,0 +1,33 @@
+package com.eneque.lab02viernes
+
+fun main() {
+    println("=========================================")
+    println("          SISTEMA DE MATRICULA")
+    println("=========================================")
+
+    print("Nombre del estudiante: ")
+    val nombreEstudiante = readLine() ?: ""
+
+    print("Cantidad de cursos: ")
+    val cantidadCursos = readLine()!!.toInt()
+
+    println()
+
+    for (i in 1..cantidadCursos) {
+        println("Curso $i")
+
+        print("Nombre del curso: ")
+        val nombreCurso = readLine() ?: ""
+
+        print("Cantidad de creditos: ")
+        val creditos = readLine()!!.toInt()
+
+        print("Valor por credito: S/ ")
+        val valorCredito = readLine()!!.toDouble()
+
+        println()
+    }
+
+    println("Datos ingresados correctamente.")
+
+}
